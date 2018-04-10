28830638b49c64d5912f055b3f42c34fb133bdb6
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2018-04-10 08:05:00 -0500

    Remove `autoIndex`

--- server/hero-model.js
+++ server/hero-model.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const heroSchema = new Schema({
+const heroSchema = new Schema(
+  {
     id: { type: Number, required: true, unique: true },
     name: String,
     saying: String
-});
+  },
+  { autoIndex: false }
+);
 
 const Hero = mongoose.model('Hero', heroSchema);
 module.exports = Hero;
