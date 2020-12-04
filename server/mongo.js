ccdeaaa964625b825756824de96ddcab77699eb8
Mahyar <mahyar.mohammadi@tietotalo.fi>
2020-12-04 10:51:25 +0200

    Mongo uri updated

--- server/mongo.js
+++ server/mongo.js
@@ -3,7 +3,7 @@ const env = require('./env/environment');
 
 mongoose.Promise = global.Promise;
 
-const mongoUri = `mongodb://${env.dbName}.documents.azure.com:${env.cosmosPort}/?ssl/=true`;
+const mongoUri = `${env.dbName}:${env.key}@${env.dbName}.mongo.cosmos.azure.com:${env.cosmosPort}/?ssl=true`;
 
 function connect() {
   return mongoose.connect(mongoUri, { auth: { user: env.dbName, password: env.key }});
